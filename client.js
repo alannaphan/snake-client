3ed89ca53090bfc16fbcc0c35c9b428bde9af3bb
Alanna <phan.alanna@gmail.com>
2023-09-28 14:20:14 -0400

    added move message for snake

--- client.js
+++ client.js
@@ -3,20 +3,29 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
+    port: 50541, // PORT number here,
   });
   // interpret incoming data as text
 
   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
+  conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: STH");
-  })
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 50);
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 150);
+  });
 
-  conn.on('data', data => {
+  conn.on("data", (data) => {
     console.log(data);
-  })
+  });
   return conn;
 };
 
